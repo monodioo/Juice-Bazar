bbbdcc981a06a2700dafbd911aacf0e167d6efa9
nguyenminhhai90 <54470213+nguyenminhhai90@users.noreply.github.com>
2019-10-04 07:00:03 +0700

    Add some file PHP
    Add files PHP: Connect DB, body(include pages), aboutUs and demo of showAll.

---
+++ templates/includes/aboutus.php
@@ -0,0 +1,47 @@
+    <div class="container-fluid site-wrap">
+      <div
+        class="page-section row"
+        style="margin-top: 5rem; margin-bottom: 5rem"
+      >
+        <div class="col-sm-12 col-md-8 px-5 pl-md-3">
+          <div class="row mb-4 h4 font-weight-bold">
+            VỀ CHÚNG TÔI
+          </div>
+          <div class="row pr-md-3">
+            <p class="text-justify">
+              Juice Bazar là thương hiệu nước ép trái cây sử dụng công nghệ
+              Cold-Pressed. Không chỉ hướng tới sản phẩm chất lượng nhất, chúng
+              tôi đầu tư và áp dụng công nghệ hàng đầu để có thể đưa nước ép tới
+              từng hộ gia đình. Phương châm kinh doanh của chúng tôi tập trung
+              vào dịch vụ cung cấp nước ép tại nhà, đặc biệt cho các gia đình
+              trẻ hiện nay.
+            </p>
+            <p class="text-justify">
+              Giữa một thế giới vận hành không ngừng, thời gian dành cho gia
+              đình là quý báu, và chúng tôi tự hào khi có thể tiết kiệm cho bạn
+              15 phút mỗi sáng mà vẫn có thể chăm lo cho sức khỏe gia đình mình.
+            </p>
+            <p class="text-justify">
+              Hãy thử sản phẩm và dịch vụ của chúng tôi để cảm nhận sự khác
+              biệt!
+            </p>
+            <p class="text-justify">
+              Juice Bazar thành lập năm 2017 bởi Huỳnh Nghĩa, Võ Thảo, Tạ Quân.
+              Chúng tôi bắt đầu từ của hàng số 20 Tống Duy Tân, Hoàn Kiếm, Hà
+              Nội và có một kế hoạch nghiêm túc về sản phẩm và dịch vụ của mình
+              khi đưa ra thị trường nước ép tại Việt Nam.
+            </p>
+            <p class="text-justify">
+              Quan điểm của chúng tôi là cung cấp các sản phẩm nước ép nguyên
+              chất nhất có thể, tập trung hoàn thiện và cải tiến các sản phẩm
+              tưởng như đơn giản nhất.
+            </p>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-4">
+          <img
+            class="img-fluid float-right rounded"
+            src="../assets/image/about us.png"
+          />
+        </div>
+      </div>
