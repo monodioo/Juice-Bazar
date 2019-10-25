f8a160773520b512d2e780cd3b68fb0334c67d38
Tuan Ngo <tuaan.nn91@gmail.com>
2019-10-25 18:53:55 +0700

    change pass

--- templates/scripts/dbconnect.php
+++ templates/scripts/dbconnect.php
@@ -1,12 +1,9 @@
 <?php
-$con=mysqli_connect("localhost","root","","juicebazar");
+$con = mysqli_connect("localhost", "id11347655_nuochoaqua", "20tongduytan", "id11347655_juicebazar");
 // Check connection
-if (!$con)
-  {
+if (!$con) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
+}
 
 // Change character set to utf8
-mysqli_set_charset($con,"utf8");
-
-?>
+mysqli_set_charset($con, "utf8");
