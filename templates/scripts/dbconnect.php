b1701affac8cc4a0897a327a523eb5e6b8f008dd
Tuan Ngo <tuaan.nn91@gmail.com>
2019-10-25 19:06:15 +0700

    Update dbconnect.php

--- templates/scripts/dbconnect.php
+++ templates/scripts/dbconnect.php
@@ -1,9 +1,11 @@
 <?php
 $con = mysqli_connect("localhost", "id11347655_nuochoaqua", "20tongduytan", "id11347655_juicebazar");
 // Check connection
-if (!$con) {
+if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
 // Change character set to utf8
 mysqli_set_charset($con, "utf8");
+
+mysqli_close($con);
