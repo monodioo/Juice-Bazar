aa43e609d979b0ef00227ab4ac5c600d22149b1a
nguyenminhhai90 <54470213+nguyenminhhai90@users.noreply.github.com>
2019-10-26 09:48:22 +0700

    Revert "Merge branch 'tuan-dev' into hai-dev"
    This reverts commit 72dad4d62b29c5fec86f41489c8ee9033ee9acf7, reversing
    changes made to 95e52c933dcc5b73b9ab6d840830f9888383affb.

--- templates/scripts/dbconnect.php
+++ templates/scripts/dbconnect.php
@@ -1,11 +1,12 @@
 <?php
-$con = mysqli_connect("localhost", "id11347655_nuochoaqua", "20tongduytan", "id11347655_juicebazar");
+$con=mysqli_connect("localhost","root","","juicebazar");
 // Check connection
-if (mysqli_connect_errno()) {
+if (!$con)
+  {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
+  }
 
 // Change character set to utf8
-mysqli_set_charset($con, "utf8");
+mysqli_set_charset($con,"utf8");
 
-mysqli_close($con);
+?>
